e00e2a8517ad74283baf03d57b93d620acc53ad3
Baeldung CI User <baeldungci@baeldung.com>
2026-07-05 06:53:03 +0000

    Replace deprecated PostgreSQLContainer import in local-dev-end (LTC M3-L2 feedback)
    org.testcontainers.containers.PostgreSQLContainer is deprecated in
    Testcontainers 2.0.3; switch to org.testcontainers.postgresql.PostgreSQLContainer
    and drop the now-unsupported generic type parameters.

--- local-dev-end/src/test/java/com/baeldung/ltc/SpringContextIntegrationTest.java
+++ local-dev-end/src/test/java/com/baeldung/ltc/SpringContextIntegrationTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.kafka.KafkaContainer;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
@@ -16,7 +16,7 @@ public class SpringContextIntegrationTest {
 
     @Container
     @ServiceConnection
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:17"))
+    static PostgreSQLContainer postgres = new PostgreSQLContainer(DockerImageName.parse("postgres:17"))
         .withInitScript("init-script.sql");
 
     @Container
